1baaf6103341117b1d01913797c1836cccdc07d9
Ruslan Prytula <ruslan.prytula@gmail.com>
2015-03-17 18:43:21 -0400

    up

--- sw.js
+++ sw.js
@@ -1,38 +1,30 @@
+importScripts("./cache.js");
+
 var CACHE_NAME = "sw-1",
-	CACHE_URLS = [
-		"./cached.html"
-	];
+    CACHE_URLS = [
+        "./cached.html"
+    ];
 
 self.addEventListener("install", function(event) {
-	// console.log("installed");
-	event.waitUntil(
-	    caches.open(CACHE_NAME)
-	      .then(function(cache) {
-	        console.log('Opened cache');
-	        return cache.addAll(CACHE_URLS);
-	      })
-	);
-	
-    // event.waitUntil(
-    // 	caches.open(CACHE_NAME).then(function(cache) {
-    // 		console.log("cache opened");
-    // 		return cache.addAll(CACHE_URLS);
-    // 	})
-    // );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log("cache opened");
+            return cache.addAll(CACHE_URLS);
+        })
+    );
 });
 
 self.addEventListener("activate", function(event) {
-	console.log("activated");
+    console.log("activated");
 });
 
 self.addEventListener("fetch", function(event) {
-	console.log("fetch");
-	event.respondWith(caches.match(event.request).then(function(response) {
-		if(response) {
-			console.log("fetched from cache");
-			return response;
-		}
+    event.respondWith(caches.match(event.request).then(function(response) {
+        if(response) {
+            console.log("fetched from cache");
+            return response;
+        }
 
-		return fetch(event.request);
-	}));
+        return fetch(event.request);
+    }));
 });
