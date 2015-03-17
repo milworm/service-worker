e9fd38a554899ea502013caccdf97534fea48227
Ruslan Prytula <ruslan.prytula@gmail.com>
2015-03-17 17:45:21 -0400

    up

--- sw.js
+++ sw.js
@@ -1,3 +1,22 @@
+var CACHE_NAME = "sw-1",
+	CACHE_URLS = [
+		"./cached.html"
+	];
+
 self.addEventListener("install", function(event) {
-    console.log("installed");
+    event.waitUntill(caches.open(CACHE_NAME).then(function(cache) {
+    	console.log("cache opened");
+    	return cache.addAll(CACHE_URLS);
+    }));
+});
+
+self.addEventListener("fetch", function(event) {
+	event.respondWith(caches.match(event.request).then(function(response) {
+		if(response) {
+			console.log("fetched from cache");
+			return response;
+		}
+
+		return fetch(event.request);
+	}));
 });
