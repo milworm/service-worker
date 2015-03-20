e9ffab78064f2a20f3b1d65cd837be749c54dd6e
Ruslan Prytula <ruslan.prytula@gmail.com>
2015-03-20 19:18:33 -0400

    up

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ window.onload = function() {
     document.querySelector("h1").innerHTML = "index.html (js)";
 }
 
-navigator.serviceWorker.register("service-worker-cache.js", {
+navigator.serviceWorker.register("sw.js", {
     scope: "./"
 }).then(function(registration) {
     console.log("success", registration);
