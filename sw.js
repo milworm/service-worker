348a47600c48cfab0b0baa313cc9ad4da074dc98
Ruslan Prytula <ruslan.prytula@gmail.com>
2015-03-20 21:11:05 -0400

    up

--- sw.js
+++ sw.js
@@ -2,7 +2,12 @@
 // which are not implemented in Chrome 40.
 Cache.prototype.add||(Cache.prototype.add=function(t){return this.addAll([t])}),Cache.prototype.addAll||(Cache.prototype.addAll=function(t){function e(t){this.name="NetworkError",this.code=19,this.message=t}var r=this;return e.prototype=Object.create(Error.prototype),Promise.resolve().then(function(){if(arguments.length<1)throw new TypeError;return t=t.map(function(t){return t instanceof Request?t:String(t)}),Promise.all(t.map(function(t){"string"==typeof t&&(t=new Request(t));var r=new URL(t.url).protocol;if("http:"!==r&&"https:"!==r)throw new e("Invalid scheme");return fetch(t.clone())}))}).then(function(e){return Promise.all(e.map(function(e,n){return r.put(t[n],e)}))}).then(function(){return void 0})}),CacheStorage.prototype.match||(CacheStorage.prototype.match=function(t,e){var r=this;return this.keys().then(function(n){var o;return n.reduce(function(n,u){return n.then(function(){return o||r.open(u).then(function(r){return r.match(t,e)}).then(function(t){return o=t})})},Promise.resolve())})});
 
-var CACHE_NAME = "challengeu";
+var VERSION = location.href.split("?")[1].match(/[0-9\.]+/),
+    CACHE_NAME = "challengeu",
+    CACHE_URLS = [
+        "./css/main.css?v=" + VERSION,
+        "./js/main.js?v=" + VERSION
+    ];
 
 /**
  * @param {Object} request
@@ -18,13 +23,20 @@ var isCacheable = function(request) {
     // cache any css/js file.
     return ["css", "js"].indexOf(url.split(".").pop()) > -1;
 }
+    
+/**
+ * @return {Promise}
+ */
+var openCache = function() {
+    return caches.open(CACHE_NAME);
+}
 
 /**
  * @param {Request} request
  * @return {Promise}
  */
 var checkInCache = function(request) {
-    return caches.open(CACHE_NAME).then(function(cache) {
+    return openCache().then(function(cache) {
         return cache.match(request);
     });
 }
@@ -35,7 +47,7 @@ var checkInCache = function(request) {
  * @return {Promise}
  */
 var putInCache = function(request, response) {
-    return caches.open(CACHE_NAME).then(function(cache) {
+    return openCache().then(function(cache) {
         return cache.put(request, response);
     });
 }
@@ -130,6 +142,12 @@ var isIndexPageRequested = function(url) {
 
     return false;
 }
+
+self.addEventListener("install", function(event) {
+    event.waitUntill(openCache().then(function(cache) {
+        cache.addAll(CACHE_URLS);
+    }));
+});
 
 self.addEventListener("fetch", function(event) {
     event.respondWith(Promise.resolve().then(function() {
