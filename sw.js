8966f22ce007e1690bb03c0a832e9352a6379f88
Ruslan Prytula <ruslan.prytula@gmail.com>
2015-03-20 22:04:11 -0400

    up

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 Cache.prototype.add||(Cache.prototype.add=function(t){return this.addAll([t])}),Cache.prototype.addAll||(Cache.prototype.addAll=function(t){function e(t){this.name="NetworkError",this.code=19,this.message=t}var r=this;return e.prototype=Object.create(Error.prototype),Promise.resolve().then(function(){if(arguments.length<1)throw new TypeError;return t=t.map(function(t){return t instanceof Request?t:String(t)}),Promise.all(t.map(function(t){"string"==typeof t&&(t=new Request(t));var r=new URL(t.url).protocol;if("http:"!==r&&"https:"!==r)throw new e("Invalid scheme");return fetch(t.clone())}))}).then(function(e){return Promise.all(e.map(function(e,n){return r.put(t[n],e)}))}).then(function(){return void 0})}),CacheStorage.prototype.match||(CacheStorage.prototype.match=function(t,e){var r=this;return this.keys().then(function(n){var o;return n.reduce(function(n,u){return n.then(function(){return o||r.open(u).then(function(r){return r.match(t,e)}).then(function(t){return o=t})})},Promise.resolve())})});
 
 var VERSION = 2,
-    CACHE_NAME = "challengeu",
+    CACHE_NAME = "challengeu" + VERSION,
     CACHE_URLS = [
         "./css/main.css?v=" + VERSION,
         "./js/main.js?v=" + VERSION
@@ -144,9 +144,25 @@ var isIndexPageRequested = function(url) {
 }
 
 self.addEventListener("install", function(event) {
-    event.waitUntil(openCache().then(function(cache) {
-        cache.addAll(CACHE_URLS);
-    }));
+    event.waitUntil(Promise.all(
+        // add static files.
+        openCache().then(function(cache) {
+            cache.addAll(CACHE_URLS);
+        }),
+
+        // cleanup old caches.
+        caches.keys().then(function(names) {
+            var methods = names.map(function(name) {
+                if (name == CACHE_NAME)
+                    return ;
+
+                console.log("Deleting out of date cache:", name);
+                return caches.delete(name);
+            });
+
+            return Promise.all(methods);
+        });
+    );
 });
 
 self.addEventListener("fetch", function(event) {
